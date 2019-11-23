e9299c93aea3e790fcbb49c0fa9ceef3c3928fd8
Dustin Paltz <dtpaltz@gmail.com>
2019-11-23 15:41:08 -0800

    Couple resolved challenges

---
+++ AmazonCodeChallenges/K_NearestPostOffices/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/*
+K Nearest Post Offices
+Find the k post offices located closest to you, given your location and a list of locations of all post offices available.
+Locations are given in 2D coordinates in [X, Y], where X and Y are integers.
+Euclidean distance is applied to find the distance between you and a post office.
+Assume your location is [m, n] and the location of a post office is [p, q], the Euclidean distance between the
+office and you is SquareRoot((m - p) * (m - p) + (n - q) * (n - q)).
+K is a positive integer much smaller than the given number of post offices. from aonecode.com
+
+e.g.
+Input
+you: [0, 0]
+post_offices: [[-16, 5], [-1, 2], [4, 3], [10, -2], [0, 3], [-5, -9]]
+k = 3
+
+Output from aonecode.com
+[[-1, 2], [0, 3], [4, 3]]
+*/
+
+
+namespace K_NearestPostOffices
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var center = new int[] { 0, 0 };
+			var pos = new int[][] { new int[] { -16, 5 }, new int[] { -1, 2 }, new int[] { 4, 3 }, new int[] { 10, -2 }, new int[] { 0, 3 }, new int[] { -5, -9 } };
+			int k = 3;
+
+			var results = SelectCloasestPostOffices(center, pos, k);
+
+			foreach (var item in results)
+			{
+				Console.WriteLine($"[{item[0]}, {item[1]}]");
+			}
+
+			Console.ReadLine();
+		}
+
+		private static List<int[]> SelectCloasestPostOffices(int[] center, int[][] pos, int k)
+		{
+			var results = new List<KeyValuePair<int[], double>>();
+			for (int j = 0; j < pos.Length; j++)
+			{
+				results.Add(new KeyValuePair<int[], double>(pos[j], EuclideanDist(center, pos[j])));
+			}
+
+			results.Sort((x, y) => x.Value.CompareTo(y.Value));
+			return results.GetRange(0, k).Select(x => x.Key).ToList();
+		}
+
+		private static double EuclideanDist(int[] p1, int[] p2)
+		{
+			int m = p1[0];
+			int n = p1[1];
+			int p = p2[0];
+			int q = p2[1];
+
+			return Math.Sqrt((m - p) * (m - p) + (n - q) * (n - q));
+		}
+	}
+}
---
+++ AmazonCodeChallenges/MoviesOnFlight/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/*
+You are on a flight and wanna watch two movies during this flight.
+You are given int[] movie_duration which includes all the movie durations.
+You are also given the duration of the flight which is d in minutes.
+Now, you need to pick two movies and the total duration of the two movies is less than or equal to (d - 30min).
+Find the pair of movies with the longest total duration. If multiple found, return the pair with the longest movie.
+
+e.g.
+Input
+movie_duration: [90, 85, 75, 60, 120, 150, 125]
+d: 250
+
+Output from aonecode.com
+[90, 125]
+90min + 125min = 215 is the maximum number within 220 (250min - 30min)
+*/
+
+namespace MoviesOnFlight
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var movie_duration = new int[] { 90, 85, 75, 60, 120, 150, 125 };
+			int d = 250;
+			var moviePair = SelectMovies(movie_duration, d);
+
+			foreach (var item in moviePair)
+			{
+				Console.WriteLine($"[{item[0]}, {item[1]}]  ==>  {item[0]}min + {item[1]}min = {item[0] + item[1]}");
+			}
+
+			Console.ReadLine();
+		}
+
+		private static List<int[]> SelectMovies(int[] movieTimes, int flightTime)
+		{
+			int targetDuration = flightTime - 30;
+			var movieSelections = new List<int[]>();
+			int bestDuration = 0;
+
+			for (int j = 0; j < movieTimes.Length; j++)
+			{
+				for (int k = 0; k < movieTimes.Length; k++)
+				{
+					if (j != k)
+					{
+						int mJ = movieTimes[j];
+						int mK = movieTimes[k];
+						int thisDuration = mJ + mK;
+
+						if (thisDuration <= targetDuration)
+						{
+							var mPair = new int[] { mJ, mK };
+							var mReversePair = new int[] { mK, mJ };
+
+							bool isUniquePair = true;
+
+							foreach (var item in movieSelections)
+							{
+								if (item.SequenceEqual(mReversePair))
+								{
+									isUniquePair = false;
+									break;
+								}
+							}
+
+							if (isUniquePair)
+							{
+								if (thisDuration > bestDuration)
+								{
+									movieSelections = new List<int[]>() { mPair };
+									bestDuration = thisDuration;
+								}
+								else if (thisDuration == bestDuration)
+								{
+									movieSelections.Add(mPair);
+								}
+							}
+						}
+					}
+				}
+			}
+
+			return movieSelections;
+		}
+	}
+}
